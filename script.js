f1e60326c12acf32f2a6073fe4092926c4e04b15
PiraticaX <darshanpiraticax@gmail.com>
2024-06-20 12:10:01 +0530

    test

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function myFunction(){
-    const arr=[10,23,2,9,43,18];
-arr.push(7);
-document.getElementById("ptag").innerHTML=arr;
-};
+
+    class Car{
+        constructor(name,year){
+            this.name=name;
+            this.year=year;
+        }
+    }
+    const Car1=new Car("Audi",2020);
+    const Car2=new Car("BMW",2011);
+
+    document.getElementById("ptag").innerHTML=Car1.name+" "+Car2.name;
+}
 
 
 
