89521b45ba8b6a8766f7289f4def7c95f31e6588
PiraticaX <darshanpiraticax@gmail.com>
2024-06-21 13:42:03 +0530

    cookies

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-var x=true;
+const ele=document.getElementById("p1");
 function myFunction(){
-const myc=document.getElementsByTagName("p");
-var len=myc.length;
-for(var i=0;x && i<len;i++){
-    setInterval(function() { 
-        myc[i].innerHTML="red";
-},1000)
-myc[i].style.color="black";
-if(i>=len){
-    i=0;
+    checkCookie();
 }
-}
-}
-function reset(){
-    x=false;
-}
-
+    function setCookie(cname,cvalue,exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+      
+      function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i < ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+      
+      function checkCookie() {
+        let user = getCookie("username");
+        if (user != "") {
+          alert("Welcome again " + user);
+        } else {
+           user = prompt("Please enter your name:","");
+           if (user != "" && user != null) {
+             setCookie("username", user, 30);
+           }
+        }
+      }
 
 
